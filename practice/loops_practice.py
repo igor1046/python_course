c88df81911e20f69fccac68583bcc0baa548bf17
igor1046 <igorpushkarev22@gmail.com>
2026-04-06 17:43:55 +0400

    initial commit

---
+++ practice/loops_practice.py
@@ -0,0 +1,28 @@
+import random, time
+
+class Solver:
+
+    def numbertask(self):
+        numbers = list(range(1,8))
+        for n in numbers:
+            print(n)
+            if n == 5:
+                break
+    
+    def wordstask(self):
+        words = [f"str{i}" for i in range(10)]
+        for word in words:
+            print(word)
+
+    def rostics_load(self):
+        count = 0
+        for i in range(10):
+            load = random.randint(0,100)
+            print(load)
+
+            if load > 85:
+                print(f"Крылышки в опасности! Нагрузка - {load}")
+                
+
+practice = Solver()
+practice.rostics_load()
